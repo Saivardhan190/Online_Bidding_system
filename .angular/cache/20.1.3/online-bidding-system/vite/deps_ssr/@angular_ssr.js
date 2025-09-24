e5d1d05f4dd21f8fed24811950eb88f259c37300
Saivardhan190 <vardhankallempudi@gmail.com>
2025-09-24 17:52:19 +0530

    frontend

---
+++ .angular/cache/20.1.3/online-bidding-system/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,42 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-AAWQLDQB.js";
+import "./chunk-EBKLBPY7.js";
+import "./chunk-DG5OCMWH.js";
+import "./chunk-CKKYU222.js";
+import "./chunk-ADOEZMQ4.js";
+import "./chunk-JVERC5TU.js";
+import "./chunk-CNGBFKFU.js";
+import "./chunk-PANNOPI5.js";
+import "./chunk-DMO44UNM.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
